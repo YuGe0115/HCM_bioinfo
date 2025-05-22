0a7aada31ac18a8c178c21e2b28c3ecd5b98dd7c
YuGe0115 <1724905827@qq.com>
2025-05-22 13:21:30 +0800

    Create run_nextflow.zsh

---
+++ run_nextflow.zsh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 运行 Sarek 工作流的 Nextflow 命令
+nextflow run /home/public/nextflow/nf-core-sarek_3.5.1/3_5_1 \
+    -profile singularity \
+    --input samplesheet.csv \
+    --outdir /scratch/merlin/test-result \
+    --genome GATK.GRCh38 \
+    --igenomes_base /data/public/Reference/igenomes \
+    --wes \
+    --intervals /data/public/Reference/Intervals/gencode.v43.annotation_exons_chrM_100bp_slopped.bed \
+    --tools deepvariant \
+    -resume
