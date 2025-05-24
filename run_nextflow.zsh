66f64ef5d326b89692686a1d85b408aa585055b9
YuGe0115 <1724905827@qq.com>
2025-05-25 00:24:25 +0800

    Update run_nextflow.zsh

--- run_nextflow.zsh
+++ run_nextflow.zsh
@@ -3,7 +3,7 @@
 # 运行 Sarek 工作流的 Nextflow 命令
 nextflow run /home/public/nextflow/nf-core-sarek_3.5.1/3_5_1 \
     -profile singularity \
-    --input /scratch/yuge/HCM_bioinfo/samplesheet.csv \
+    --input /data/yuge/0523new_try/samplesheet.csv \
     --outdir /data/yuge/0523new_try \
     --genome GATK.GRCh38 \
     --igenomes_base /data/public/Reference/igenomes \
