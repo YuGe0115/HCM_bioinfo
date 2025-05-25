17fd3459ac2a83e439a4069d6fe4b67b4af2e7c9
YuGe0115 <1724905827@qq.com>
2025-05-26 01:22:15 +0800

    Update vep.zsh

--- vep.zsh
+++ vep.zsh
@@ -4,8 +4,8 @@
 
 ref_path=/data/public/Reference
 
-singularity exec /home/public/singularity/ensembl-vep.sif vep \
-    --offline --cache --dir_cache ${ref_path}/VEP \   # 使用离线模式和缓存
+singularity exec /home/public/singularity/ensembl-vep.sif vep 
+    --offline --cache --dir_cache ${ref_path}/VEP    # 使用离线模式和缓存
     --species homo_sapiens --cache_version 113 --assembly GRCh38  # 指定人类GRCh38基因组
     --dir_plugins ${ref_path}/VEP/plugins             # 插件目录
     --gencode_basic --exclude_predicted --pick        # 使用基础Gencode注释，排除预测结果，选择最佳注释
@@ -18,6 +18,7 @@ singularity exec /home/public/singularity/ensembl-vep.sif vep \
     --plugin dbscSNV,${ref_path}/dbscSNV/dbscSNV1.1_GRCh38.txt.gz 
     --plugin pLI,${ref_path}/ExACpLI/ExACpLI_values.txt 
     --plugin LoFtool,${ref_path}/LoFtool/LoFtool_scores.txt 
+    --plugin Phenotypes,/data/yuge/0523new_try/Phenotypes/phenotype_feature.txt
     --plugin dbNSFP,${ref_path}/dbNSFP/dbNSFP4.9a_grch38.gz,MutationTaster_pred,Polyphen2_HDIV_pred,LRT_pred,MutationAssessor_pred,PROVEAN_pred 
     --input_file /data/yuge/0523new_try/vcf_merge/all_samples_merged.vcf.gz               # 输入文件
     --output_file /data/yuge/0523new_try/vcf_merge/all_samples_merged_annotated.vcf.gz  # 输出注释后的VCF
